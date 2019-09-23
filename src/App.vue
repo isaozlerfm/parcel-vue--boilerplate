ef892e71caf0aa7cbb06f1368803451a4356fec0
Isa Ozler <isa.ozler@frontmen.nl>
2019-09-23 14:34:26 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hello {{bundler}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { aMixin } from './services';
+
+export default Vue.extend({
+  mixins: [ aMixin ],
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    color: green;
+  }
+</style>
---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+export const aMixin = {
+  created() {
+    console.log('app created');
+  },
+  mounted() {
+    console.log("app mounted");
+  }
+};
